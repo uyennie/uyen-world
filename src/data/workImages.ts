5a7b8b0467e4b7e1cd98241ffad95eadb09d883e
uyennie <dienuyenh@gmail.com>
2024-01-11 00:59:17 +1100

    fix draggable disappearing bug

--- src/data/workImages.ts
+++ src/data/workImages.ts
@@ -6,84 +6,24 @@ interface WorkImageData {
 
 const workImages: WorkImageData[] = [
   {
-    url: "/assets/works/arena-ios-icon.png",
-    id: "are:na",
-    category: "code design",
+    url: "https://cdn.britannica.com/79/65379-050-5CF52BAC/Shortfin-mako-shark-seas.jpg",
+    id: "Conscious",
+    category: "uiux",
   },
   {
-    url: "/assets/works/20XX_poster.webp",
-    id: "20XX",
+    url: "/assets/works/digital-self-4.jpg",
+    id: "d.selfhood",
     category: "art design",
   },
   {
-    url: "/assets/works/arith_desktop.webp",
-    id: "arith",
-    category: "code",
-  },
-  {
-    url: "/assets/works/everyday_trash.webp",
-    id: "everyday__",
-    category: "art",
-  },
-  {
-    url: "/assets/works/famicase.webp",
-    id: "famicase",
-    category: "art design",
-  },
-  {
-    url: "/assets/works/figma_plugin.webp",
-    id: "f-c-d",
+    url: "https://img.freepik.com/free-vector/cute-shark-eating-bone-waving-hand-cartoon-vector-icon-illustration-animal-nature-isolated-flat_138676-6427.jpg?size=338&ext=jpg&ga=GA1.1.1412446893.1704758400&semt=ais",
+    id: "Curology",
     category: "code design",
   },
   {
-    url: "/assets/works/goodreads_overview.webp",
-    id: "goodreads",
-    category: "design",
-  },
-  {
-    url: "/assets/works/graphite_allegory.webp",
-    id: "g-r",
-    category: "art",
-  },
-  {
-    url: "/assets/works/henesys_tags.webp",
-    id: "henesys",
+    url: "/assets/works/content_1.webp",
+    id: "CONTENT",
     category: "code design",
-  },
-  {
-    url: "/assets/works/misc_water.webp",
-    id: "misc.",
-    category: "design art",
-  },
-  {
-    url: "/assets/works/pidgin.webp",
-    id: "pidgin",
-    category: "code",
-  },
-  {
-    url: "/assets/works/pokesearch_duo.webp",
-    id: "pokéSearch",
-    category: "code",
-  },
-  {
-    url: "/assets/works/s-i-l_grid.webp",
-    id: "s-i-l",
-    category: "code",
-  },
-  {
-    url: "/assets/works/y0p_veiled.webp",
-    id: "y0p",
-    category: "code",
-  },
-  {
-    url: "/assets/works/yhhu_xyz_desktop.webp",
-    id: "yhhu.xyz",
-    category: "code design art",
-  },
-  {
-    url: "/assets/works/ZINEDEF_stories.webp",
-    id: "ZINEDEF",
-    category: "art design",
   },
 ];
 
